80fe7124af68792a526e3ccefbad9d79b7e08856
André Rodrigues <andre360.ars@gmail.com>
2022-02-18 17:24:21 -0300

    Requisito 6 - Usa o metodo .map, junto com o componente PlanetCard, para renderizar os planetas na tela

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    const planetList = planets
+      .map(({ name, image }) => (<PlanetCard
+        planetName={ name }
+        planetImage={ image }
+        key={ name }
+      />));
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planetList}
       </div>
     );
   }
