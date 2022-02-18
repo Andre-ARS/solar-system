4ed022845b2106be4b8098dd274267b60a61c6d1
André Rodrigues <andre360.ars@gmail.com>
2022-02-18 16:36:24 -0300

    Requisito 3 - Cria componente Title que renderiza um titulo com o texto baseado em uma prop

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <p>kkk</p>
+        <Title headline="Planetas" />
       </div>
     );
   }
